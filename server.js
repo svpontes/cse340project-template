9ae67b011c009b1fde003c1200932a4028052e6b
svpontes <svpontes2015@gmail.com>
2025-09-04 20:51:04 +0100

    stating project cse340

--- server.js
+++ server.js
@@ -6,14 +6,31 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
+
+/* ***********************
+ * view engine and templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") //not at views root
+
 /* ***********************
  * Routes
  *************************/
+
+
 app.use(static)
+//index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
+
+
 
 /* ***********************
  * Local Server Information

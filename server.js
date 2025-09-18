e5a24767dc0bb8efbbf6bb183e43e5bd739f05a4
svpontes <svpontes2015@gmail.com>
2025-09-18 21:38:29 +0100

    add robust kecth error

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use("/inv", inventoryRoute)
   res.render("index", {title: "Home"})
 })*/
 
-app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
@@ -59,10 +59,12 @@ app.listen(port, () => {
 *************************/
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav();
+  let message;
   console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
-    message: err.message,
-    nav,
+    message,
+    nav
   });
 });
